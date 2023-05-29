43ef4b261b68cb0f9c9a903ffa5727e2167139c6
Evgeniy <zhenya.todorov@gmail.com>
2023-05-29 09:43:20 +0300

    fix6

--- service/calendar-service.js
+++ service/calendar-service.js
@@ -5,27 +5,27 @@ class CalendarService {
     this.SCOPE_CALENDAR = 'https://www.googleapis.com/auth/calendar';
     this.SCOPE_EVENTS = 'https://www.googleapis.com/auth/calendar.events';
     this.auth = null;
-    this.authenticate()
-      .then(res => (this.auth = res))
-      .catch(e => console.log(e));
+    // this.authenticate()
+    //   .then(res => (this.auth = res))
+    //   .catch(e => console.log(e));
     this.calendar = googleCalendar.calendar('v3');
   }
 
-  async authenticate() {
-    try {
-      const jwtClient = new googleCalendar.auth.JWT(
-        process.env.GOOGLE_CLIENT_EMAIL,
-        null,
-        process.env.GOOGLE_PRIVATE_KEY,
-        [this.SCOPE_CALENDAR, this.SCOPE_EVENTS]
-      );
-      console.log('authenticate', process.env.GOOGLE_CLIENT_EMAIL, this.SCOPE_CALENDAR, this.SCOPE_EVENTS, jwtClient)
-      await jwtClient.authorize();
-      return jwtClient;
-    } catch (e) {
-      console.log(e);
-    }
-  }
+  // async authenticate() {
+  //   try {
+  //     const jwtClient = new googleCalendar.auth.JWT(
+  //       process.env.GOOGLE_CLIENT_EMAIL,
+  //       null,
+  //       process.env.GOOGLE_PRIVATE_KEY,
+  //       [this.SCOPE_CALENDAR, this.SCOPE_EVENTS]
+  //     );
+  //     console.log('authenticate', process.env.GOOGLE_CLIENT_EMAIL, this.SCOPE_CALENDAR, this.SCOPE_EVENTS, jwtClient)
+  //     await jwtClient.authorize();
+  //     return jwtClient;
+  //   } catch (e) {
+  //     console.log(e);
+  //   }
+  // }
 
   async create(event, calendarId) {
     try {
